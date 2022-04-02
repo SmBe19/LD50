418786e62460ebb18122d9561eae8f426ef71aaa
Benjamin Schmid <git@schmidb.ch>
2022-04-02 17:49:29 +0100

    Local Multiplayer for TicTacToe

--- entropy-rally/src/App.js
+++ entropy-rally/src/App.js
@@ -1,10 +1,19 @@
 import { Client } from 'boardgame.io/react';
 import { TicTacToe } from './Game';
 import { TicTacToeBoard } from './Board'
+import { Local } from "boardgame.io/multiplayer";
 
-const App = Client({
+const TicTacToeClient = Client({
     game: TicTacToe,
-    board: TicTacToeBoard
-});
+    board: TicTacToeBoard,
+    multiplayer: Local(),
+})
+
+const App = () => (
+    <div>
+        <TicTacToeClient playerID="0" />
+        <TicTacToeClient playerID="1" />
+    </div>
+);
 
 export default App;
